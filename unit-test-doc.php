48ada3a0d0914fe04fc45258c82147138c6f6fa3
David Callizaya <davidcallizaya@gmail.com>
2018-03-27 11:22:51 -0400

    fix vendor loader for command line.

--- unit-test-doc.php
+++ unit-test-doc.php
@@ -1,8 +1,8 @@
 <?php
 if (file_exists(__DIR__ . '/vendor/autoload.php')) {
 	require __DIR__ . '/vendor/autoload.php';
-} elseif (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
-	require __DIR__ . '/../../vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/../../autoload.php')) {
+	require __DIR__ . '/../../autoload.php';
 }
 
 $project = empty($argv[1]) ? '.' : $argv[1];
