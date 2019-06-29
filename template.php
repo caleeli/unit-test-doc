70cddfdda47a4588c5d8beb6eb01571f854edee3
David Callizaya <davidcallizaya@gmail.com>
2019-06-29 07:04:40 -0400

    Add bootstrap and diagrams

--- template.php
+++ template.php
@@ -1,5 +1,5 @@
     <div id="<?php echo htmlentities($indexTitle, ENT_QUOTES); ?>">
-        <h1><?php echo $card['title']; ?></h1>
+        <h3><a href="#<?php echo htmlentities($indexTitle, ENT_QUOTES); ?>"><?php echo $card['title']; ?></a></h3>
         <p><?php echo $card['description']; ?></p>
         <table border="1" cellpadding="0" cellspacing="0" width="100%">
             <tr>
@@ -14,10 +14,10 @@
             <tr>
                 <td><b>Pre-conditions</b></td>
                 <td>
-                    <?php foreach($card['pre-conditions'] as $preCondition => $steps): ?>
+                    <?php foreach ($card['pre-conditions'] as $preCondition => $steps): ?>
                     <?php echo $preCondition; ?>
                     <ol>
-                        <?php foreach($steps as $step): ?>
+                        <?php foreach ($steps as $step): ?>
                         <li><?php echo $step[0]; ?></li>
                         <?php endforeach; ?>
                     </ol>
@@ -27,20 +27,31 @@
         </table>
         <table border="1" cellpadding="0" cellspacing="0" width="100%">
             <tr>
-                <td width="50%"><b>Steps</b></td>
-                <td width="50%"><b>Expected results</b></td>
+                <td width="60%"><b>Steps</b></td>
+                <td width="40%"><b>Expected results</b></td>
             </tr>
-            <?php foreach($card['steps'] as $i => $steps):?>
+            <?php foreach ($card['steps'] as $i => $steps):?>
             <tr>
                 <td>
                     <ul>
-                        <?php foreach($steps as $step): ?>
-                        <li><?php echo $step[0]; ?></li>
+                        <?php foreach ($steps as $step): ?>
+                        <li><?php echo $step[0]; ?>
+                            <div class="card bg-light pt-3 mt-3 mb-3 mr-3">
+                                <code><?php echo highlightText($step[1]); ?></code>
+                            </div>
+                        </li>
                         <?php endforeach; ?>
                     </ul>
                 </td>
                 <td><ul><li><?php echo implode('</li><li>', $card['results'][$i]); ?></li></ul></td>
             </tr>
             <?php endforeach; ?>
+            <?php if (!empty($card['diagram'])): ?>
+            <tr>
+                <td colspan="2">
+                    <?php echo $card['diagram']; ?>
+                </td>
+            </tr>
+            <?php endif; ?>
         </table>
     </div>
